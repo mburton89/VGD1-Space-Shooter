1f9c69aa6967c3fe123bdff97752db663f368fee
WEBSTERAD\WUStudent <dts@webster.edu>
2022-04-13 16:38:24 -0500

    Ovals
    Moon in oval rotation

---
+++ Space Shooter/Assets/Code/OvalRotationScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OvalRotationScript : MonoBehaviour
+{
+    float alpha = 0f;
+    public float RotSpeed;
+    float rotation = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotateWiredly();
+    }
+
+    public void rotateWiredly()
+    {
+        //2ways
+        //make 5 points to touch
+        //oval pattern and rotatting**
+
+        transform.position = new Vector2(0f + (20f * Mathf.Sin(Mathf.Deg2Rad * alpha)), 15f + (25f * Mathf.Cos(Mathf.Deg2Rad * alpha)));
+        alpha += RotSpeed;//can be used as speed
+    }
+}
